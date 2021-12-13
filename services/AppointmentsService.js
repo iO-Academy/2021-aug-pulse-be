85e85335669ff8e86dc801492a410b36257e5b7b
Ben <benjamindhayden@gmail.com>
2021-12-13 16:08:10 +0000

    small comment

--- services/AppointmentsService.js
+++ services/AppointmentsService.js
@@ -1,6 +1,8 @@
 const AppointmentRepository = require('../repositories/AppointmentsRepository');
 // const {ObjectId} = require("mongodb");
 
+
+//DO DATA THINGS HERE!!!!
 class AppointmentsService {
     async getAvailableAppointments(todo) {
         // let id = {"_id":new ObjectId(todo.params.id)};

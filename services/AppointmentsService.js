cb0aa9ff1aa5fab4ff61fdec0d4793d338b5e54f
Max <maximusdyer97@gmail.com>
2021-12-14 16:11:28 +0000

    added validation functions

--- services/AppointmentsService.js
+++ services/AppointmentsService.js
@@ -1,13 +1,16 @@
 const AppointmentRepository = require('../repositories/AppointmentsRepository');
 const logger = require('../logger/logger');
+const bodyParser = require("body-parser");
+const Ajv = require("ajv");
+const ajv = new Ajv()
+// const additionalProperties = require("ajv/lib/vocabularies/applicator/additionalProperties");
 // const {ObjectId} = require("mongodb");
-
 
 //DO DATA THINGS HERE!!!!
 class AppointmentsService {
     async getAvailableAppointments(req) {
         // let id = {"_id":new ObjectId(todo.params.id)};
-        const doctorappointments = {"doctorID":Number(req.query.doctorid), "date":Number(req.query.appdate)};
+        const doctorappointments = {"doctorID": Number(req.query.doctorid), "date": Number(req.query.appdate)};
         logger.info("ID passed to getAppointments: " + doctorappointments.doctorID);
         logger.info("Date passed to getAppointments: " + doctorappointments.date);
         return await AppointmentRepository.getAppointments(doctorappointments);
@@ -26,15 +29,35 @@ class AppointmentsService {
     }
 
     async bookAppointment(req) {
-        logger.info("booked new appointment: " + req.body);
-        try {
-            const bookapp =  req.body;
-            return await AppointmentRepository.bookAppointment(bookapp);
-        } catch (err) {
-            logger.error(`booking new appointment error: ${err}`);
+        const checkFields = {
+            type: "object",
+            properties: {
+                "date": {type: "integer"},
+                "timeID": {type: "integer"},
+                "doctorID": {type: "integer"},
+                "patientFirstName": {type: "string"},
+                "patientLastName": {type: "string"},
+                "patientEmail": {type: "string"},
+                "notes": {type: "string"}
+            },
+            required: ["date", "timeID", "doctorID", "patientFirstName", "patientLastName", "patientEmail", "notes"],
+            additionalProperties: false
+        }
+        const validate = ajv.compile(checkFields)
+
+        const valid = validate(req.body);
+
+        if (valid) {
+            try {
+                const bookapp = req.body;
+                logger.info("booked new appointment");
+                return await AppointmentRepository.bookAppointment(bookapp);
+            } catch (err) {
+                logger.error(`booking new appointment error: ${err}`);
+            }
+        } else {
+            logger.error('Could not add new appointment due to invalid input data. (not json, missing props or type error)');
         }
     }
-
-    }
-
+}
 module.exports = new AppointmentsService;

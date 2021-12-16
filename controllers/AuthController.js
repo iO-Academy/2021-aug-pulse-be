df3a426cf7eef20f8547266f09de5b8b77b261f3
Ben <benjamindhayden@gmail.com>
2021-12-16 14:16:37 +0000

    auth feature now resonds with a cookie if successful or 401 error if not.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -11,7 +11,16 @@ class AuthController {
 
     comparePass(req, res, next) {
         logger.info('Controller: get doc login')
-        return AuthService.comparePass(req).then(result => res.send(result));
+        return AuthService.comparePass(req)
+            .then((result) => {
+                logger.info(`Auth success = ${result}`);
+                if (result === true) {
+                    return res.cookie('isAuthed', true, { domain: '', sameSite: "Lax", path: '/auth', expires: new Date(Date.now() + 900000)}).send();
+                }
+                else {
+                    res.status(401).end();
+                }
+            });
     }
 }
 
